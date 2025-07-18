7d242c35f733570d08f18932dae8234418e6a590
Nurfaizzi <ocitbmc@gmail.com>
2025-07-18 19:11:54 +0700

    commit two

--- src/app/Components/navbar/index.js
+++ src/app/Components/navbar/index.js
@@ -1,15 +1,13 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from "next/image";
-import { Search } from 'lucide-react';
+import Image from "next/image"
 import Banneratas from "../Banneratas"
 const Navbar = () => {
   return (
     <nav className="bg-[url(/hero.jpg)] ...  h-100 bg-cover">
       <div className="mx-auto max-w-9xl px-2 sm:px-6 lg:px-8 bg-blue-800 h-100 opacity-75">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
           <div className="flex flex-1  sm:items-stretcht">
             <div className="flex shrink-0 items-center gap-2">
               <Image
